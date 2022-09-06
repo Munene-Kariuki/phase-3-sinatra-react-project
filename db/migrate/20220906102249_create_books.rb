ad5119ba95e8c08181097b0110a15dd998b7a644
Denis <denismunene999@gmail.com>
2022-09-06 13:42:52 +0300

    create books and authors tables

--- db/migrate/20220906102249_create_books.rb
+++ db/migrate/20220906102249_create_books.rb
@@ -1,4 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
+    create_table :books do |t|
+      t.string :name
+      t.string :description
+      t.integer :price
+      t.integer :author_id
+    end  
   end
 end
