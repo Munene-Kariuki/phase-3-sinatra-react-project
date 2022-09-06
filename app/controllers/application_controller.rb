8602effb1267bb445e6d6c46e8ced2d604627bbc
Denis <denismunene999@gmail.com>
2022-09-06 17:56:56 +0300

    Add database querries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #Get requests
   get '/books' do
     books = Book.all
     books.to_json(include: :author)
@@ -26,6 +27,7 @@ class ApplicationController < Sinatra::Base
     book.to_json(include: :author)
   end  
 
+  #Post requests
   post '/books' do 
     book = Book.create(
       name: params[:name],
@@ -34,6 +36,27 @@ class ApplicationController < Sinatra::Base
       price: params[:price]
     )
     book.to_json
+  end  
+
+  post '/authors' do 
+    author = Author.create(
+      name: params[:name],
+      about: params[:about]
+    )
+    author.to_json
+  end  
+
+  #Delete requests
+  delete '/books/:id' do
+    book = Book.find(params[:id])
+    book.destroy
+    book.to_json
+  end 
+
+  delete '/authors/:id' do
+    author = Author.find(params[:id])
+    author.destroy
+    author.to_json
   end  
 
 end
